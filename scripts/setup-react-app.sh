0785e27bc6619020e090a625c178050376c40c95
Aditya Wanere <aditya.wanere2000@gmail.com>
2025-07-30 00:02:38 +0530

    Adds Sudo

--- scripts/setup-react-app.sh
+++ scripts/setup-react-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PRE_SIGNED_URL="__signed_url__"
+PRE_SIGNED_URL="https://adi-s3-bucket-01.s3.ap-south-1.amazonaws.com/latest-builds/react-app-latest.tar?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAS4MTC57YH7UOBG4W%2F20250729%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20250729T181321Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=b98395dc6db061573b08a0616ea0163b8f2a6fc8f57a994d44a3017721398cd3"
 TAR_FILE="/tmp/react-app-latest.tar"
 IMAGE_NAME="react-app"
 
@@ -13,14 +13,14 @@ if ! command -v docker &> /dev/null; then
 fi
 
 # Download Docker image from S3 (pre-signed URL)
-curl -o $TAR_FILE "$PRE_SIGNED_URL"
+sudo curl -o $TAR_FILE "$PRE_SIGNED_URL"
 
 # Load Docker image
-docker load -i $TAR_FILE
+sudo docker load -i $TAR_FILE
 
 # Stop previous container
-docker stop react-app || true
-docker rm react-app || true
+sudo docker stop react-app || true
+sudo docker rm react-app || true
 
 # Run the container
-docker run -d --name react-app -p 80:80 $IMAGE_NAME:latest
+sudo docker run -d --name react-app -p 80:80 react-app:latest
