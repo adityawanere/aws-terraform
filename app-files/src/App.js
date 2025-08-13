f8348ba1e5f285bc7a2429fc39e0da86b7272db2
Aditya Wanere <69197711+adityawanere@users.noreply.github.com>
2025-08-13 14:18:55 +0530

    Update App.js

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 👌
+          Learn React
         </a>
       </header>
     </div>
